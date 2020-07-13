ef595cd36a45500be54a4391c936e9c073bd6e03
Lashima <45839126+Lashima@users.noreply.github.com>
2020-07-14 02:01:59 +0400

    Updates

---
+++ precache-manifest.d6b2659d9f3ae9d08f1b2da9419c146b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69dd8455d8871364cdb8fb5a8c2c88b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69a1a2a7abff77c43ae7",
+    "url": "/static/css/2.0a5287ed.chunk.css"
+  },
+  {
+    "revision": "11ac7128b0ae77fff7cc",
+    "url": "/static/css/main.c17bc185.chunk.css"
+  },
+  {
+    "revision": "69a1a2a7abff77c43ae7",
+    "url": "/static/js/2.f81fd722.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.f81fd722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ac7128b0ae77fff7cc",
+    "url": "/static/js/main.a4aabe3a.chunk.js"
+  },
+  {
+    "revision": "bb00a6b87c7b408d6910",
+    "url": "/static/js/runtime-main.85fa02e7.js"
+  }
+]);
